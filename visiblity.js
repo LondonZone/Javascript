221a2fe6274dd52ac13ecad7a3f354b43711c05b
David London <londonzone21@gmail.com>
2015-08-05 20:55:55 -0400

    initial commit

---
+++ visiblity.js
@@ -0,0 +1,24 @@
+
+//definining Function - click listiner
+ document.onclick = function() {
+    var previous;
+     $("div[title='main'] li").click(function(){
+
+        // this targets the specific element that you clicked on
+        $(this).addClass("visible") 
+        previous = $(this);
+
+    });
+     previous.removeClass('visible');
+     //ensure visibiltiy is changed after another tab is clicked
+    
+    //show all publicaitions 
+    if($('#view-all').click(function())){
+        $("div[title='main'] li").removeClass("hidden");
+    });
+
+}
+
+
+
+
