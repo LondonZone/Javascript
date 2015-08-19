4e267c8d27affc9e2ae21d4818b110acb037d442
David London <londonzone21@gmail.com>
2015-08-19 11:38:22 -0400

    second commit, base case working for filtering

--- visiblity.js
+++ visiblity.js
@@ -1,25 +1,51 @@
 //definining Function - click listiner
 $(document).ready(function(){
-    var previous=$('section#recent');
+    
+    //Global Variable Declarations
+    var previous=$('section#recent'); //Base case
     var myClass;
-
+    var sectionId; //Array of Section Id's
+    
      $("ul[title='main'] > li").click(function(){
         alert("hello world");
+
         // this targets the specific element that you clicked on
-        //$(this).addClass("visible")
+        //$(this).addClass("visible") 
+
         myClass = $(this).attr("class");
         console.log(myClass);
         $('section#'+myClass).removeClass('hidden').addClass('visible');
-        previous = $(this);
+        //previous = $(this);
         console.log(previous);
-
+        previous.removeClass('visible').addClass('hidden');
     });
+    
+    //show all publicaitions 
+    /*$('#view-all').click(function(){
+        $("ul[title='main'] > li").removeClass("hidden").addClass('visible');
+         previous = $(this);
+   });*/
 
-    //show all publicaitions
-    //$('#view-all').click(function(){
-     //   $("ul[title='main'] > li").removeClass("hidden");
-       //  previous = $(this);
-   // });
+    
+    //TODO get previous working
+    //ensure visibiltiy is changed after another tab is clicked
+    sectionId = ['recent', '2014', '2013', '2012', '2011','2010'];
+
+    for ( var i = 0; i < sectionId.length; i++ ){
+
+        var currentClass = sectionId[i];
+
+        if ($(currentClass).hasClass('visible'))
+        {
+            previous = sectionId[i];
+            console.log(previous);
+            break;  
+        }
+    }
+
     previous.removeClass('visible');
-     //ensure visibiltiy is changed after another tab is clicked
+    console.log(previous);
+
 }); // end of function
+
+
